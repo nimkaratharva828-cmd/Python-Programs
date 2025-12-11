7a644a2dd721dec77de46d06f90104b5cace82bd
nimkaratharva828-cmd <nimkaratharva828@gmail.com>
2025-12-11 16:21:40 +0530

    Daily update

--- dec11.py
+++ dec11.py
@@ -14,3 +14,44 @@ print(f"The length of {g} is {len(g)}")
 cnt = g.count(v)
 print(f"The count of {v} using count function is {cnt}")
 
+print(f"The uppercase of {g} is {g.upper()}") # Doing to uppercase using upper function
+print(f"The lowercase of {g} is {g.lower()}") # Doing to lowercase using lower function
+print(f"The titlecase of {g} is {g.title()}") # Doing to titlecase using title function
+print(f"The casefold of {g} is {g.casefold()}") # Doing to casefold using casefold function
+print(f"The capitalized of {g} is {g.capitalize()}") # Doing to capitalized using capitalize function
+print(f"The swappedcase of {g} is {g.swapcase()}") # Doing to swappedcase using swapcase function
+print(f"The startwith 'W' of {g} is {g.startswith('W')}")
+print(f"The endswith 'y' of {g} is {g.endswith('y')}")
+
+words = s.split(" ")
+print(len(words)) 
+print(words)
+
+newstring = "#".join(words)
+print(newstring)
+
+s1 = "insta"
+s2 = "gram"
+s3 = s1 + s2
+print(s3)
+s4 = s1.join(s2)
+print(s4)
+
+s1.index("i")
+s1.isalpha()
+s1.isalnum()
+s1.isdecimal()
+s1.islower()
+s1.isdigit()
+s1.isidentifier()
+
+
+countg = 0
+spm = "Instagram"
+# in operator
+print("z" in "Python") #False
+vowels = "aeiouAEIOU"
+for ch in spm:
+    if ch in vowels:
+        countg = countg + 1
+print(f"Total vowels in {spm} are {countg}")
