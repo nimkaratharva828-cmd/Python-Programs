0b3d30831d94211330411c7a74537de108e92e1a
nimkaratharva828-cmd <nimkaratharva828@gmail.com>
2026-01-14 14:37:57 +0530

    Daily update

---
+++ jan14.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,a,loc):
+        self.age = a
+        self.location = loc
+
+s1 = Student(21,"Wardha")
+a="Atharva Sir"
+print(type(a))
+print(s1)
+print(type(s1))
+print(id(s1))
+print(s1.age)
+print(s1.location)
