2185f797451e4be9e70a53b6d29cf9deff0a7151
nimkaratharva828-cmd <nimkaratharva828@gmail.com>
2026-02-12 14:21:18 +0530

    Added new basic file

--- OOP WS/file_handling.py
+++ OOP WS/file_handling.py
@@ -1,3 +1,25 @@
-f = open("demo2.py", 'r')
-execute = 10 + 20
-print(execute)
+# Read the entire file
+f = open('file_handling.txt', 'r')
+print(f.read())
+f.close()
+
+print()
+print()
+
+# Read the file line by line
+f = open('file_handling.txt', 'r')
+for line in f:
+    print(line.strip())
+f.close()
+
+print()
+print()
+
+# Append to the file
+f = open('file_handling.txt', 'a')
+f.write('This is an appended line.')
+f.close()
+f = open('file_handling.txt', 'r')
+print(f.read())
+f.close()
+
